d869e2c00920e66fa8535c10fc68f1d1ff3659dd
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-15 17:01:17 +0530

    Implemented Many-to-Many (through Join Table) & One-to-One Relationships; Created a New  Migration for the New Changes;

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -11,11 +11,19 @@ namespace SamuraiApp.Data
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }      //Added a Table called Battles so that we can directly interact with it if needed in the Future.
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(
                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });      //This creates a Composite Key of SamuraiID and BattleID and this helps the EF Core identify the presence of a Many-to-Many Realtionship between Samurai and Battle Tables
+            modelBuilder.Entity<Horse>().ToTable("Horses");     //Rather than using DbSet (not used DbSet to indicate the Fact that it is not needed in our Business Logic; so that no one uses it directly)
+                                                                //to name the Table we have used the ToTable Function. If we dont use this Function, by default Table name will be Horse (singular as compared to Plural Names we have used so far for all our tables)
         }
     }
 }
