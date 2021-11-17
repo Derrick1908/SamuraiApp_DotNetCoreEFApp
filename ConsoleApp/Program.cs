9ce14d8b6bb990154bb141ea91ddc7030559141e
Derrick Nazareth <derrick.nazareth@teknorix.com>
2021-11-17 09:53:20 +0530

    Added Code for Eager Loading (Include) / Projecting (Select) / Explicit Loading (Load) of Related Data;Filtering and Sorting Related Data; Querying 1:1 and 1:M relationships; Context Tracking and Non Tracking (Entry Method)

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,7 @@
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ConsoleApp
@@ -11,25 +12,55 @@ namespace ConsoleApp
         private static SamuraiContext _context = new SamuraiContext();
         static void Main(string[] args)
         {
-            //_context.Database.EnsureCreated();           //Ensures that a Database Exist. If it does not then it Creates the corresponding Database.
-            //GetSamurais("Before Add:");
-            //AddSamurai();
-            //GetSamurais("After Add:");
-            //InsertMultipleSamurais();
-            //InsertVariousTypes();
-            //GetSamuraisSimpler();
-            //QueryFilters();
-            //GetSamurais("After Add:");
-            //RetrieveAndUpdateSamurai();
-            //RetrieveAndUpdateMultipleSamurais();
-            //MultipleDatabaseOperations();
-            //RetrieveAndDeleteASamurai();
-            //InsertBattle();
-            //QueryAndUpdateBattle_Disconnected();
+            #region Update/Delete/Retrieve Operations; Filtering/Aggregating Queries
+            /*
+            _context.Database.EnsureCreated();           //Ensures that a Database Exist. If it does not then it Creates the corresponding Database.
+            GetSamurais("Before Add:");
+            AddSamurai();
+            GetSamurais("After Add:");
+            InsertMultipleSamurais();
+            InsertVariousTypes();
+            GetSamuraisSimpler();
+            QueryFilters();
+            GetSamurais("After Add:");
+            RetrieveAndUpdateSamurai();
+            RetrieveAndUpdateMultipleSamurais();
+            MultipleDatabaseOperations();
+            RetrieveAndDeleteASamurai();
+            InsertBattle();
+            QueryAndUpdateBattle_Disconnected(); */
+            #endregion
+
+            //InsertNewSamuraiWithAQuote();
+            //InsertNewSamuraiWithManyQuotes();
+            //AddQuoteToExistingSamuraiWhileTracked();
+            //AddQuoteToExistingSamuraiNotTracked(2);
+            //AddQuoteToExistingSamuraiNotTracked_Easy(2);
+            //EagerLoadSamuraiWithQuotes();
+            //ProjectSomeProperties();
+            //ProjectSamuraiWithQuotes();
+            //ExplicitLoadQuotes();
+            //FilteringWithRelatedData();
+            //ModifyingRelatedDataWhenTracked();
+            //ModifyingRealtedDataWhenNotTracked();
+            //JoinBattleAndSamurai();
+            //EnlistSamuraiIntoABattle();
+            //GetSamuraiWithBattles();
+            //RemoveJoinBetweenSamuraiAndBattleSimple();
+            //AddNewSamuraiWithHorse();
+            //AddNewHorseToSamuraiUsingId();
+            //AddNewHorseToSamuraiObject();
+            //AddNewHorseToDisconnectedSamuraiObject();
+            //ReplaceHorse();
+            //GetSamuraisWithHorse();
+            //GetHorsewithSamurai();
+            GetSamuraiWithClan();
+            GetClanWithSamurais();
             Console.Write("Press any Key...");
             Console.ReadKey();
         }
 
+        #region Insert, Update, Delete Operations; Filtering/Aggregating Queries
         private static void InsertMultipleSamurais()
         {
             var samurai = new Samurai { Name = "Sampson" };
@@ -141,6 +172,270 @@ namespace ConsoleApp
                 newContextInstance.Battles.Update(battle);          //All Properties are sent for Update in a Disconnected Update Operation inspite of only one property being changed, as it has no tracking to see what has changed. It only knows that something has changed.
                 newContextInstance.SaveChanges();
             }
+        }
+
+        #endregion
+        private static void InsertNewSamuraiWithAQuote()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Kambei Shimada",
+                Quotes = new List<Quote>
+                {
+                    new Quote { Text = "I've come to save you" }
+                }
+            };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void InsertNewSamuraiWithManyQuotes()
+        {
+            var samurai = new Samurai
+            {
+                Name = "Kyuzo",
+                Quotes = new List<Quote>
+                {
+                    new Quote { Text = "Watch out for my Sharp Sword!" },
+                    new Quote { Text = "I told you to watch out for the sharp sword! Oh Well!" }
+                }
+            };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void AddQuoteToExistingSamuraiWhileTracked()
+        {
+            var samurai = _context.Samurais.FirstOrDefault();
+            samurai.Quotes.Add(new Quote
+            {
+                Text = "I bet you're happy that I've Saved you!"
+            });
+            _context.SaveChanges();
+        }
+
+        private static void AddQuoteToExistingSamuraiNotTracked(int samuraiId)
+        {
+            var samurai = _context.Samurais.Find(samuraiId);
+            samurai.Quotes.Add(new Quote
+            {
+                Text = "Now that I saved you, will you feed me dinner?"
+            });
+            using(var newContext = new SamuraiContext())
+            {
+                //newContext.Samurais.Update(samurai);             //Note that even though its a new context, it will check and see that the Child(Quote) has no Key, so it will add it a new Record with the Foreign Key as the Parent Key . Since we are calling the Update it will update the Samurai also.     
+                newContext.Samurais.Attach(samurai);            //Rather than updating the Samurai we just attach the Samurai, so it will only update the Quote Object and not the Samurai Again.
+                newContext.SaveChanges();               
+            }
+        }
+
+        private static void AddQuoteToExistingSamuraiNotTracked_Easy(int samuraiId)
+        {
+            var quote = new Quote
+            {
+                Text = "Now that I saved you, will you feed me dinner again?",
+                SamuraiId = samuraiId
+            };
+            using(var newContext = new SamuraiContext())
+            {
+                newContext.Quotes.Add(quote);
+                newContext.SaveChanges();
+            }
+        }
+
+        private static void EagerLoadSamuraiWithQuotes()
+        {
+            var samuraiWithQuotes = _context.Samurais.Where(s => s.Name.Contains("Julie"))
+                                                     .Include(s => s.Quotes)
+                                                     .Include(s => s.Clan)
+                                                     .FirstOrDefault();
+        }
+
+        private static void ProjectSomeProperties()
+        {
+            var someProperties = _context.Samurais.Select(s => new { s.Id, s.Name }).ToList();
+            var idsAndNames = _context.Samurais.Select(s => new IdAndName { Id = s.Id, Name = s.Name }).ToList();
+        }
+
+        //Creating a Data Type of Ids and Names for depicting anonymous vs Data Type in Select/Projection.
+        public struct IdAndName
+        {
+            public IdAndName(int id, string name)
+            {
+                Id = id;
+                Name = name;
+            }
+            public int Id;
+            public string Name;
+        }
+
+        private static void ProjectSamuraiWithQuotes()
+        {
+            var somePropertiesWithQuotes = _context.Samurais
+                .Select(s => new { s.Id, s.Name, s.Quotes })
+                .ToList();
+            var somePropertiesWithQuotes2 = _context.Samurais
+                .Select(s => new { s.Id, s.Name, s.Quotes.Count })
+                .ToList();
+            var somePropertiesWithQuotes3 = _context.Samurais
+                .Select(s => new {s.Id, s.Name,
+                HappyQuotes = s.Quotes.Where(q => q.Text.Contains("happy"))})
+                .ToList();
+            var samuraisWithHappyQuotes = _context.Samurais
+                .Select(s => new
+                {
+                    Samurai = s,
+                    HappyQuotes = s.Quotes.Where(q => q.Text.Contains("happy"))         //An Example of how with Select you can filter out certain part of the other Attribute (i.e. Filtering the Related Data) as compared to the Includes, which gets all the Data
+                }).ToList();
+
+            //var firstSamurai = samuraisWithHappyQuotes[0].Samurai.Name += " The Happiest";      //Just Modifying Locally and not saving changes to show how even though anonymous types are not tracked during projection, if Entities are a part of the anonymous type they are tracked
+                                                                                                //Kept a Watch on _context.ChangeTracker.Entries -> Results View
+        }
+
+        private static void ExplicitLoadQuotes()
+        {
+            var samurai = _context.Samurais.FirstOrDefault(s => s.Name.Contains("Julie"));
+            _context.Entry(samurai).Collection(s => s.Quotes).Load();       //Collection becoz there exists a one-to-many realtionship between Samurai and Quotes
+            _context.Entry(samurai).Reference(s => s.Horse).Load();         //Reference becoz there exist a one-to-one relationship between Samurai and Horse
+        }
+
+        private static void FilteringWithRelatedData()
+        {
+            var samurais = _context.Samurais
+                                   .Where(s => s.Quotes.Any(q => q.Text.Contains("happy")))
+                                   .ToList();
+        }
+
+        private static void ModifyingRelatedDataWhenTracked()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes).FirstOrDefault();
+            samurai.Quotes[0].Text = " Did you hear that?";
+            _context.Quotes.Remove(samurai.Quotes[2]);
+            _context.SaveChanges();
+        }
+
+        private static void ModifyingRealtedDataWhenNotTracked()
+        {
+            var samurai = _context.Samurais.Include(s => s.Quotes).FirstOrDefault();
+            var quote = samurai.Quotes[0];
+            quote.Text = "Did you hear that again?";
+            using (var newContext = new SamuraiContext())
+            {
+                //newContext.Quotes.Update(quote);        //This Updates all the Quotes of the Samurai as well as the Update of Samurai Details, since each quote is Linked to a Samurai (and this Samurai internally has Quotes attached to it)
+                newContext.Entry(quote).State = EntityState.Modified;       //Here we are explicitly setting the state of that Paritcular QUote Object to Modify. This is done without affecting the states of other Linked Attributes like Samurai etc. Hence during Update now, only the Quote is Updated as expected.
+                newContext.SaveChanges();
+            }
+        }
+
+        private static void JoinBattleAndSamurai()
+        {
+            //Samurai and Battle already exist and we have their Ids
+            var sbJoin = new SamuraiBattle { SamuraiId = 2, BattleId = 1 };
+            _context.Add(sbJoin);       //Note that since we dont have a DBContext of SamuraiBattle we cannot add it against that through DBContext i.e. _context.SamuraiBattle.Add(). This _context.Add() method allows us to directly add to the DB without DBSet
+            _context.SaveChanges();
+        }
+        
+        private static void EnlistSamuraiIntoABattle()
+        {
+            var battle = _context.Battles.Find(1);
+            battle.SamuraiBattles
+                .Add(new SamuraiBattle { SamuraiId = 3 });      //The Context will automatically pick the Battle Id from the current active one and add it into the Samurai Battle
+            _context.SaveChanges();
+        }
+
+        private static void GetSamuraiWithBattles()
+        {
+            var samuraiWithBattle = _context.Samurais
+                .Include(s => s.SamuraiBattles)
+                .ThenInclude(sb => sb.Battle)
+                .FirstOrDefault(samurai => samurai.Id == 2);
+
+            var samuraiWithBattlesCleaner = _context.Samurais.Where(s => s.Id == 2)
+                .Select(s => new
+                {
+                    Samurai = s,
+                    Battle = s.SamuraiBattles.Select(sb => sb.Battle)
+                }).FirstOrDefault();
+                                             
+        }
+
+        private static void RemoveJoinBetweenSamuraiAndBattleSimple()
+        {
+            var join = new SamuraiBattle { BattleId = 1, SamuraiId = 2 };
+            _context.Remove(join);
+            _context.SaveChanges();
+        }
+
+        private static void AddNewSamuraiWithHorse()
+        {
+            var samurai = new Samurai { Name = "Jina Ujichika" };
+            samurai.Horse = new Horse { Name = "Silver" };
+            _context.Samurais.Add(samurai);
+            _context.SaveChanges();
+        }
+
+        private static void AddNewHorseToSamuraiUsingId()
+        {
+            var horse = new Horse { Name = "Scout", SamuraiId = 2 };
+            _context.Add(horse);
+            _context.SaveChanges();
+        }
+
+        private static void AddNewHorseToSamuraiObject()
+        {
+            var samurai = _context.Samurais.Find(1);
+            samurai.Horse = new Horse { Name = "Black Beauty" };
+            _context.SaveChanges();
+        }
+
+        private static void AddNewHorseToDisconnectedSamuraiObject()
+        {
+            var samurai = _context.Samurais.AsNoTracking().FirstOrDefault(s => s.Id == 4);
+            samurai.Horse = new Horse { Name = "Mr. Ed" };
+            using (var newContext = new SamuraiContext())
+            {
+                newContext.Attach(samurai);         //Becoz the Horse does not have an ID, EF Core understands that it is the one to be Inserted and not the Samurai who already has an Id (This is done throught the Attach Function)
+                newContext.SaveChanges();
+            }
+        }
+
+        private static void ReplaceHorse()
+        {
+            var samurai = _context.Samurais.Include(s => s.Horse).FirstOrDefault(s => s.Id == 4);
+            //var samurai = _context.Samurais.Find(4);            //Has a Horse, but since the Horse is not loaded in memory, it cannot delete the Old Horse and when i tries to insert the New Horse it gets an Exception of Duplicate Key
+                                                                  //Above Statement will give an Error.  
+            samurai.Horse = new Horse { Name = "Trigger" };     //Since our DB is defined in such a way that a Horse cannot exist without a Samurai due to Foreign Key Relationship, therefore it first deletes the old Horse and then Inserts the New Horse.
+            _context.SaveChanges();
+        }
+
+        private static void GetSamuraisWithHorse()
+        {
+            var samurai = _context.Samurais.Include(s => s.Horse).ToList();
+        }
+
+        private static void GetHorsewithSamurai()       //This part is a liitle tricky becoz the Horse does not have a Navigational Property back to Samurais i.e. it does not have public Samurai samurai; it only has the Samurai Id
+        {
+            var horseWithoutSamurai = _context.Set<Horse>().Find(3);    //Set Property helps to create a Temp Set in this case as we do not have a DBSet of Horse. This will just get the Horse and not associated Samurais
+
+            var horseWithSamurai = _context.Samurais.Include(s => s.Horse)      //Start with the Samurais and Include the Horse and then Filter the one whose Id is 3. Gets a Single Horse with its corresponding Samurai only.
+                .FirstOrDefault(s => s.Horse.Id == 3);
+
+            var horseWithSamurais = _context.Samurais           //Gets all the Samurais who have Horses.
+                .Where(s => s.Horse != null)
+                .Select(s => new { Horse = s.Horse, Samurai = s })
+                .ToList();
+        }
+
+        private static void GetSamuraiWithClan()
+        {
+            var samurai = _context.Samurais.Include(s => s.Clan).FirstOrDefault();
+        }
+
+        private static void GetClanWithSamurais()
+        {
+            //var clan = _context.Clans.Include(c => c.??)  //This is not Possible as we dont have a Navigational Property to Samurais
+            var clan = _context.Clans.Find(1);
+            var samuraisForClan = _context.Samurais.Where(s => s.Clan.Id == 1).ToList();
         }
     }
 }
---
+++ SamuraiApp.Data/SamuraiContextNoTracking.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using SamuraiApp.Domain;
+
+namespace SamuraiApp.Data
+{
+    public class SamuraiContextNoTracking: DbContext
+    {
+        public SamuraiContextNoTracking()
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        public DbSet<Samurai> Samurais { get; set; }
+        public DbSet<Quote> Quotes { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<Battle> Battles { get; set; }      //Added a Table called Battles so that we can directly interact with it if needed in the Future.
+
+        public static readonly ILoggerFactory ConsoleLoggerFactory
+            = LoggerFactory.Create(builder =>                                   //Adds a Logger to the Context Class
+            {
+                builder
+                .AddFilter((category, level) =>
+                category == DbLoggerCategory.Database.Command.Name              //Filter only the Database Commands
+                && level == LogLevel.Information)                               //and the Level of Detail needed sp we select only Basic Level of Log Level and nothing more.
+                .AddConsole();
+            });
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                .UseLoggerFactory(ConsoleLoggerFactory)
+                .EnableSensitiveDataLogging()           //This will display also sensitive data on the Log, which by default is not logged (Incoming Parameter Values)
+                .UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });      //This creates a Composite Key of SamuraiID and BattleID and this helps the EF Core identify the presence of a Many-to-Many Realtionship between Samurai and Battle Tables
+            modelBuilder.Entity<Horse>().ToTable("Horses");     //Rather than using DbSet (not used DbSet to indicate the Fact that it is not needed in our Business Logic; so that no one uses it directly)
+                                                                //to name the Table we have used the ToTable Function. If we dont use this Function, by default Table name will be Horse (singular as compared to Plural Names we have used so far for all our tables)
+        }
+    }
+}
